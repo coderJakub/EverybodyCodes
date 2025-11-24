de4be3ff6caeb8b701e57e5c1efe5b21b96e49fb
coderJakub <kliemann.jakub@gmail.com>
2025-11-24 14:54:27 +0100

    day10/2025

--- 2025 - The Song of Ducks and Dragons/Day10/p3.py
+++ 2025 - The Song of Ducks and Dragons/Day10/p3.py
@@ -1,16 +1,49 @@
 import sys
+from functools import cache
 
 fileName = sys.argv[1]
 with open(fileName) as f:
-    content = f.read()
-    
-data = content
-R = len(data)
-#C = len(data[0])
+    content = f.read().splitlines()
 
-res = 0
-for el in data:
-    continue
+hides = set()
+sheeps = []
+dragon = None
+for i, line in enumerate(content):
+    for j, x in enumerate(line):
+        if x=='#': hides.add((i,j))
+        if x=='S': sheeps.append((i,j))
+        if x=='D': dragon = (i,j)
+sheeps = tuple(sheeps)
 
+R = len(content)
+L = len(content[0])
 
-print(res)
+def getMovesD(x,y):
+    movesD = [(-2,-1), (-2,1), (-1,2), (1,2), (2,1), (2,-1), (1,-2), (-1,-2)]
+    moves = []
+    for xx, yy in movesD:
+        nx, ny = x+xx,y+yy
+        if 0 <= nx < R and 0 <= ny < L:
+            moves.append((nx,ny))
+    return moves
+
+@cache
+def dfs(sheeps: tuple[tuple], dragon: tuple, turn='S'):
+    res = 0
+    changes = 0
+    if turn == 'S':
+        if not sheeps: return 1
+        for i,(x,y) in enumerate(sheeps):
+            nx = x+1
+            if nx == R: 
+                changes += 1
+            elif (nx,y) != dragon or (nx,y) in hides:
+                changes += 1
+                res += dfs(tuple(sheeps[:i] + ((nx,y),) + sheeps[i+1:]), dragon, 'D')
+    if turn == 'D' or changes == 0:
+        x,y = dragon
+        for nx,ny in getMovesD(x,y):
+            res += dfs(tuple(s for s in sheeps if s != (nx,ny) or s in hides), (nx,ny), 'S')
+    return res
+                            
+print(dfs(sheeps, dragon))
